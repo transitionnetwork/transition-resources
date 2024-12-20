40d7c08665abc6476d53e3f1f314dec5d9072f2d
Christian Wach <needle@haystack.co.uk>
2024-12-20 11:04:03 +0000

    Remove unnecessary string concatenation

--- assets/templates/embed/embed-resource-fields.php
+++ assets/templates/embed/embed-resource-fields.php
@@ -146,7 +146,7 @@ defined( 'ABSPATH' ) || exit;
 <?php
 
 $tax_args = [
-	'before'        => '<div class="taxonomy-container">' . '<h4>' . esc_html__( 'Categories &amp; Tags', 'transition-resources' ) . '</h4>',
+	'before'        => '<div class="taxonomy-container"><h4>' . esc_html__( 'Categories &amp; Tags', 'transition-resources' ) . '</h4>',
 	'template'      => '<span class="taxonomy-label">%s:</span> <span class="taxonomy-term-list">%l.</span>',
 	'term_template' => '<a href="%1$s" rel="tag">%2$s</a>',
 	'sep'           => '<br />',
