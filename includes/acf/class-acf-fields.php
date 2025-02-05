993cbe9e2aca54521e7692bfa1f487fee9e6f491
Christian Wach <needle@haystack.co.uk>
2025-02-05 14:05:21 +0000

    JetPack class is in global namespace

--- includes/acf/class-acf-fields.php
+++ includes/acf/class-acf-fields.php
@@ -606,8 +606,8 @@ class Fields {
 	public function markdown_render( $value, $post_id, $field ) {
 
 		// Add support for Jetpack Markdown.
-		if ( class_exists( 'WPCom_Markdown' ) ) {
-			$markdown = WPCom_Markdown::get_instance();
+		if ( class_exists( '\WPCom_Markdown' ) ) {
+			$markdown = \WPCom_Markdown::get_instance();
 			return wpautop( $markdown->transform( $value ) );
 		}
 
